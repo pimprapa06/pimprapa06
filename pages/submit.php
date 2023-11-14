9371836b71fbf515acbeaffb72ead73ac2cd31c6
pimprapa06 <pimprapa.da2006@email.com>
2023-11-14 10:59:13 +0700

    111

---
+++ pages/submit.php
@@ -0,0 +1,24 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+$city = $_POST['city'];
+$web = $_POST['web'];
+$role = $_POST['role'];
+$sign = $_POST['sign'];
+
+
+echo 'email: '.$email.'<br>';
+echo 'password: '.$password.'<br>';
+echo 'city: '.$city.'<br>';
+echo 'web: '.$web.'<br>';
+echo 'role: '.$role.'<br>';
+echo 'sign: '.$sign.'<br>';
+echo 'sign1: '.$sign1.'<br>';
+echo 'sign2: '.$sign2.'<br>';
+echo 'sign3: '.$sign3.'<br>';
+
+
+
+
+?>
+
